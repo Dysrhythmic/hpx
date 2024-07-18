8c1ab82034aa152c27a35b1ee4f534f165d58885
dysrhythmic <jaketucker7@gmail.com>
2024-07-18 13:40:32 -0500

    add compare_exchange_strong()

--- libs/core/concurrency/include/hpx/concurrency/detail/lockfree_uint128_type.hpp
+++ libs/core/concurrency/include/hpx/concurrency/detail/lockfree_uint128_type.hpp
@@ -41,4 +41,32 @@
     );
     return result;
   }
+
+  template<>
+  inline bool std::atomic<hpx::lockfree::uint128_type>::compare_exchange_strong(
+    hpx::lockfree::uint128_type& expected,
+    hpx::lockfree::uint128_type desired,
+    std::memory_order memOrder
+  )
+  {
+    (void)memOrder;
+    bool result = false;
+    asm
+    (
+    "lock cmpxchg16b %1\n\t"
+    "setz %0"
+    :
+    "=r" (result),
+    "+m" (*reinterpret_cast<hpx::lockfree::uint128_type*>(this)),
+    "+d" (expected.right),                                            // high
+    "+a" (expected.left)                                              // low
+    :
+    "c" (desired.right),                                              // high
+    "b" (desired.left)                                                // low
+    :
+    "cc",
+    "memory"
+    );
+    return result;
+  }
 #endif                                                                // !defined(HPX_WITH_CXX11_ATOMIC_128BIT_LOCKFREE)
