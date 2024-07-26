fe6083b10de3a60bbd24c405fdfbff92073633f9
Dysrhythmic <jaketucker7@gmail.com>
2024-07-25 20:53:36 -0500

    add new header files to CMakeLists.txt

--- libs/core/concurrency/tests/unit/lockfree_128uint.cpp
+++ libs/core/concurrency/tests/unit/lockfree_128uint.cpp
@@ -4,9 +4,8 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#include <hpx/concurrency/detail/lockfree_uint128_type.hpp>
 #include <hpx/modules/testing.hpp>
-
-#include "libs/core/concurrency/include/hpx/concurrency/detail/lockfree_uint128_type.hpp"
 
 #include <atomic>
 #include <iostream>
